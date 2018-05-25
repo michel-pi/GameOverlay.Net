c1404ca5cd9c31bc32a2511bc409ef654b24477b
Yato <michel.pioch@gmx.de>
2018-05-25 18:05:23 +0200

    added Library class for version and author info

---
+++ Yato.DirectXOverlay/Library.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+
+namespace Yato.DirectXOverlay
+{
+    public static class Library
+    {
+        public static string Author
+        {
+            get
+            {
+                return "Yato";
+            }
+        }
+
+        public static string Name
+        {
+            get
+            {
+                return "Yato.DirectXOverlay";
+            }
+        }
+
+        public static string URL
+        {
+            get
+            {
+                return "https://github.com/YatoDev/Yato.DirectXOverlay";
+            }
+        }
+
+        public static string Version
+        {
+            get
+            {
+                try
+                {
+                    Assembly assembly = Assembly.GetExecutingAssembly();
+                    AssemblyName assemblyName = assembly.GetName();
+
+                    return assemblyName.Version.ToString();
+                }
+                catch
+                {
+                    return "1.0.0.0";
+                }
+            }
+        }
+    }
+}
