ec823529a8a875e078103e870fdfa34a1e8eef8d
Yato <michel.pioch@gmx.de>
2018-05-25 18:02:18 +0200

    added obfuscation settings

---
+++ Yato.DirectXOverlay/ObfuscationSettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Prevent string interning
+[assembly: CompilationRelaxations(CompilationRelaxations.NoStringInterning)]
+
+// Standard features
+
+[assembly: Obfuscation(Feature = "rename serializable symbols", Exclude = false)]
+[assembly: Obfuscation(Feature = "sanitize resources", Exclude = false)]
+[assembly: Obfuscation(Feature = "ildasm suppression", Exclude = false)]
+
+// Advanced features
+
+[assembly: Obfuscation(Feature = "string encryption", Exclude = false)]
+[assembly: Obfuscation(Feature = "code control flow obfuscation", Exclude = false)]
+[assembly: Obfuscation(Feature = "encrypt resources [compress]", Exclude = false)]
