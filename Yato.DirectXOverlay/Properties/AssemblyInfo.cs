1b23c2770efa8fb5c28014694f3c717b51d36548
Yato <michel.pioch@gmx.de>
2018-06-03 16:46:06 +0200

    idk

--- Yato.DirectXOverlay/Properties/AssemblyInfo.cs
+++ Yato.DirectXOverlay/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following set of attributes.
 // Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyTitle("Yato.DirectXOverlay")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Yato.DirectXOverlay")]
+[assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
