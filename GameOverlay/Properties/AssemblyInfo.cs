e1cd14260bfb5530bed7b7552d81502ed4f16350
Michel <github@chromadigital.group>
2018-08-14 02:51:23 +0200

    build

--- GameOverlay/Properties/AssemblyInfo.cs
+++ GameOverlay/Properties/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.7.7.6800")]
+[assembly: AssemblyVersion("2.8.8.6800")]
