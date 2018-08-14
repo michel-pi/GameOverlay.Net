6473708defb70ac5c3b4f59599e1220c270cf6cc
Michel <github@chromadigital.group>
2018-08-14 02:37:46 +0200

    build

--- Example/GameOverlayExample/Properties/AssemblyInfo.cs
+++ Example/GameOverlayExample/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.19.19.6800")]
-[assembly: AssemblyFileVersion("1.19.19.6800")]
+[assembly: AssemblyVersion("1.20.20.6800")]
+[assembly: AssemblyFileVersion("1.20.20.6800")]
