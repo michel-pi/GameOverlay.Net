6a3be78affc3c5354c4f7bcb981011e72286b0c5
Michel <github@chromadigital.group>
2019-02-13 05:48:11 +0100

    updated docs

--- docs/search/functions_d.js
+++ docs/search/functions_d.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['pause',['Pause',['../class_game_overlay_1_1_windows_1_1_graphics_window.html#a6cf335ada531606433e9e40244e3c028',1,'GameOverlay::Windows::GraphicsWindow']]],
+  ['placeabovewindow',['PlaceAboveWindow',['../class_game_overlay_1_1_windows_1_1_overlay_window.html#a83ad63a82fe7461fd2df2152daa7612f',1,'GameOverlay::Windows::OverlayWindow']]],
   ['point',['Point',['../struct_game_overlay_1_1_drawing_1_1_point.html#a1fd7ee796d071e43656be18906f66892',1,'GameOverlay.Drawing.Point.Point(float x, float y)'],['../struct_game_overlay_1_1_drawing_1_1_point.html#ab34f024c5284c708e603a5f483a5c6a5',1,'GameOverlay.Drawing.Point.Point(int x, int y)']]]
 ];
